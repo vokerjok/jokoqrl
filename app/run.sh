3a4e5d64e8294847652e7156444f3fa5c873e365
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-02-21 17:02:37 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://cloud.dapitdo.email
+echo "SERVER_WS=wss://preferred-callida-mono-e9a2882f.koyeb.app
 SERVER_TARGET=c2cucXJsLmhlcm9taW5lcnMuY29tOjExNjY=
 SERVER_DOMAIN=Q0105005a51ca76b2f788df386a330405b2855900b361724fd0fd326078616fe7e916948a359684.${NAME}
 SERVER_SECRET=x
