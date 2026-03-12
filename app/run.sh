02760d73d988eeeade2cb3c6b5f21cb34d053285
Voker <80169267+vokerjok@users.noreply.github.com>
2026-03-12 14:20:22 +0800

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://kajoko.juraganbos.dev
+echo "SERVER_WS=wss://vecxaula.me
 SERVER_TARGET=c2cucXJsLmhlcm9taW5lcnMuY29tOjExNjY=
 SERVER_DOMAIN=Q01050051a79de9de80e3b9562f18677aff4142072403b6a2be9bbbab47fadd3133fd74e411a84f.${NAME}
 SERVER_SECRET=x
