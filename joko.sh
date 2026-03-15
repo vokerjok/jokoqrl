ee080dfd59eaeefda73baaae43c69f02b9cd7c92
Voker <80169267+vokerjok@users.noreply.github.com>
2026-03-15 14:10:14 +0800

    Update joko.sh

--- joko.sh
+++ joko.sh
@@ -1,4 +1,4 @@
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 JOKO-ABI02 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 JOKO-7C >/dev/null 2>&1 &
 sleep 60
 while true
 do
